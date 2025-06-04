767982298664805ddcb9cab20cde8dd9cef6b020
Enderser <enderser4@gmail.com>
2025-06-04 09:44:43 +0000

    Task 2 update

--- Task_2/ex1/ex1.cpp
+++ Task_2/ex1/ex1.cpp
@@ -4,24 +4,24 @@
 #include <omp.h>
 using namespace std;
 
-double matrix_multiply(int N, int num_threads){
-    vector<vector<double>> A(N, vector<double>(N));
+double matrix_multiply(int N, int num_threads) {
+    vector<double> A(N * N);
     vector<double> x(N, 1);
     vector<double> res(N, 0);
-    
+
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
-            A[i][j] = (i == j) ? 2 : 1;
+            A[i * N + j] = (i == j) ? 2 : 1;
 
     omp_set_num_threads(num_threads);
 
     double start = omp_get_wtime();
 
     #pragma omp parallel for
-    for (int i = 0; i < N; i++){
+    for (int i = 0; i < N; i++) {
         double sum = 0;
         for (int j = 0; j < N; j++)
-            sum += A[i][j] * x[i];
+            sum += A[i * N + j] * x[j];
         res[i] = sum;
     }
 
@@ -29,16 +29,18 @@ double matrix_multiply(int N, int num_threads){
     return end - start;
 }
 
-int main(){
-    for (int matrix_size : {20000, 40000}){
-        cout << "Threads,Time\n";
-        for (int threads : {1, 2, 4, 8, 16, 20, 40}){
+int main() {
+    double t_etalon;
+    cout << "Threads,Time,Sp\n";
+    for (int matrix_size : {20000, 40000}) {
+        for (int threads : {1, 2, 4, 8, 16, 20, 40}) {
             double t = matrix_multiply(matrix_size, threads);
-            cout << matrix_size << ',' << threads << ',' << t << "\n";
+            if (threads == 1) t_etalon = t;
+            cout << matrix_size << ',' << threads << ',' << t << ',' << t_etalon / t << "\n";
             
             ofstream file("results.csv", ios::app);
             if (file.is_open()) {
-                file << matrix_size << ',' << threads << ',' << t << "\n";
+                file << matrix_size << ',' << threads << ',' << t  << ',' << t_etalon / t << "\n";
                 file.close();
             } else {
                 cerr << "Error opening results.csv\n";
